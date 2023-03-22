b1c182fc6151c9418a03952b24cb57869ad1dd13
Yorick <yorick.devleeschouwer@gmail.com>
2023-03-22 11:58:38 +0100

    fixed some basic files

---
+++ bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+    spl_autoload_register(function($class){
+        require_once(__DIR__ . DIRECTORY_SEPARATOR . "classes" . DIRECTORY_SEPARATOR . $class . ".php");
+    });
+?>
--- index.php
+++ index.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
     <title>Desity - Home</title>
 </head>
 <body>
-    
+    <?php include_once("bootstrap.php"); ?>
+    <p>test</p>
 </body>
 </html>
