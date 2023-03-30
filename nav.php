039ae92a998d168f37e4648c7d9aed2c0552f075
Yorick <yorick.devleeschouwer@gmail.com>
2023-03-30 14:59:32 +0200

    changed some nav stuff

--- nav.php
+++ nav.php
@@ -1,8 +1,8 @@
 <nav>
     <ul>
         <li><h1>CoPoll</h1></li>
-        <li><a href="#"><h1>Test 1</h1></a></li>
-        <li><a href="#"><h1>Test 2</h1></a></li>
+        <li><a href="#"><h1>Mechelen</h1></a></li>
+        <li><a href="#"><h1>Account</h1></a></li>
         <li><a href="#"><h1>Test 3</h1></a></li>
         <li><a href="#"><h1>Test 4</h1></a></li>
         <li><a href="login.php"><h1>Log in</h1></a></li>
