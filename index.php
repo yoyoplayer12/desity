47a9e019436c73b4aa3831a173b0c6ff4d07b511
Yorick <yorick.devleeschouwer@gmail.com>
2023-04-19 14:53:50 +0200

    STYLING

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="icon" href="assets/brand/Logo-favicon-white.svg" style="height:40px" type="image/svg+xml">
+    <title>Copoll</title>
+</head>
+<body>
+    <?php include_once(__DIR__ . "/nav.php"); ?>
+    <div class="grid-container-index">
+        <div class="grid-item-index">
+            <h1>An inspirational title</h1>
+            <p style="width: 400px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam reprehenderit sed vitae exercitationem, corrupti, unde autem culpa perferendis dignissimos consequuntur, quod temporibus nostrum odio perspiciatis eum sequi aut nihil. Esse.</p>
+        </div>
+        <div>
+            <img src="https://placehold.co/640" alt="Placeholder">
+        </div>
+    </div>
+</body>
+</html>
--- nav.php
+++ nav.php
@@ -1,18 +1,15 @@
 <nav>
+    <img class="navlogo" src="assets/brand/logo-horizontal.svg" alt="Copoll logo" style="height: 48px;">
     <ul>
-        <li><h1>CoPoll</h1></li>
-        <li><a href="dashboard.php"><h1>Dashboard</h1></a></li>
-        <!-- residents section -->
-        <?php if(isset($_SESSION['admin']) && $_SESSION['admin'] === true): ?>
-            <li><a href="residents.php"><h1>Residents</h1></a></li>
-        <?php endif; ?>
-        <li><a href="login.php"><h1><?php
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+        <li><a href="login.php"><?php
         if (isset($_SESSION["firstname"])) {
-            echo $_SESSION["firstname"] . " " . $_SESSION["lastname"];
+            echo "Dashboard";
         }
         else{
             echo "Log in";
         }
-        ?></h1></a></li>
+        ?></a></li>
     </ul>
 </nav>
