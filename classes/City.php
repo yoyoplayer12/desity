3b8532661d25975333b4fe6973f9d17a264f4321
Yorick <yorick.devleeschouwer@gmail.com>
2023-05-03 17:00:03 +0200

    feat: you can now view open projects on your dashboard

--- classes/City.php
+++ classes/City.php
@@ -6,4 +6,12 @@
         $result = $statement->fetchAll(PDO::FETCH_ASSOC);
         return $result;
     }
+    public static function getCityById($id){
+        $conn = Db::getInstance();
+        $statement = $conn->prepare("SELECT * FROM city WHERE id=:id AND active=1");
+        $statement->bindValue(":id", $id);
+        $statement->execute();
+        $result = $statement->fetch(PDO::FETCH_ASSOC);
+        return $result;
+    }
 }
--- nav.php
+++ nav.php
@@ -1,7 +1,7 @@
-<nav>
+<nav class="sitenav">
     <a href="index.php"><img class="navlogo" src="assets/brand/logo-horizontal.svg" alt="Copoll logo" style="height: 48px;"></a>
     <ul>
-        <li><a href="services.php">SERVICES</a></li>
+        <li><a href="dashboard.php">SERVICES</a></li>
         <li><a href="about.php">ABOUT</a></li>
         <li><a href="contact.php">CONTACT</a></li>
         <li><a href="login.php" class="dashboardbutton-index">DASHBOARD</a></li>
