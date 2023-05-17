aa6ed87f3e998d09f6ac9e68972d687705a00202
Yorick <yorick.devleeschouwer@gmail.com>
2023-05-17 14:49:43 +0200

    fix: fixing chat

--- ajax/send_message.action.php
+++ ajax/send_message.action.php
@@ -1,9 +1,16 @@
 <?php
 // Establish database connection
-
+require_once('../bootstrap.php');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $message = $_POST['message'];
+    $pid = $_POST['pid'];
 
     // Insert the new message into the database
+    $conn = Db::getInstance();
+    $statement = $conn->prepare("INSERT INTO messages(`sender_id`, `project_id`, `content`) VALUES (:senderid,:projectid,:content)");
+    $statement->bindValue(":senderid", $_SESSION['userid']);
+    $statement->bindValue(":projectid", $pid);
+    $statement->bindValue(":content", $message);
+    $statement->execute();
 }
 ?>
