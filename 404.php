1a0f1ad7cb4f8b04be49e7b17d2ec6a8a4621907
Yorick <yorick.devleeschouwer@gmail.com>
2023-05-18 23:36:50 +0200

    feat: 404 test

---
+++ 404.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 - Page not found</title>
+</head>
+<body>
+    <?php include_once(__DIR__ . "/navs/nav.php"); ?>
+    <h1>404 - Page not found</h1>
+    <p>Sorry, the page you are looking for does not exist.</p>
+    <?php include_once(__DIR__ . "/footer/footer.php"); ?>
+</body>
+</html>
