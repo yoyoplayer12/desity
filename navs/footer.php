666075194a4df73b687fcd096b89745c5b64752d
Yorick <yorick.devleeschouwer@gmail.com>
2023-05-18 17:53:46 +0200

    styling: landingpage is now fully finished (some links still remain unlinked)

---
+++ navs/footer.php
@@ -0,0 +1,27 @@
+<?php
+    $image = new Image();
+    $url = $image->getUrl();
+?>
+<footer>
+    <div class="footercontainer">
+        <div style="margin-bottom:45px;">
+            <img src="<?php echo $url."assets/brand/y1uiedctgpxpgbl85xcv" ?>" alt="Copoll logo" style="width:304px;height:auto;">
+        </div>
+        <div>
+            <a href="register.php" class="footer-getstarted">GET STARTED</a>
+        </div> 
+        <div class="footer-minimenu-container">
+            <a href="#"><h5>COMPANY</h5></a>
+            <a href="#"><h5>SOCIAL</h5></a>
+            <a href="index.php#about#"><p class="body-bold-normal">About</p></a>
+            <a href="#"><p class="body-bold-normal">LinkedIn</p></a>
+            <a href="index.php#pricing"><p class="body-bold-normal">Pricing</p></a>
+            <a href="#"><p class="body-bold-normal">Twitter</p></a>
+            <a href="contact.php"><p class="body-bold-normal">Contact</p></a>
+            <a href="#"><p class="body-bold-normal">Facebook</p></a>
+        </div>
+        <div class="body-Large" style="height:30px;margin-top:48px;">
+            <p>© 2023 copoll. All right reserved | <a href="#">Support</a> | <a href="#">Privacy Policy</a> | <a href="#">Terms of Use</a> | <a href="#">Cookie Policy</a</p>
+        </div>
+    </div>
+</footer>
--- navs/nav.php
+++ navs/nav.php
@@ -5,9 +5,11 @@
 <nav class="sitenav">
     <a href="index.php"><img class="navlogo" src="<?php echo $url."assets/brand/y1uiedctgpxpgbl85xcv.svg" ?>" alt="Copoll logo" style="height: 48px;"></a>
     <ul>
-        <li><a href="dashboard.php">SERVICES</a></li>
-        <li><a href="about.php">ABOUT</a></li>
+        <li><a href="index.php#about">ABOUT</a></li>
+        <li><a href="index.php#pricing">PRICING</a></li>
         <li><a href="contact.php">CONTACT</a></li>
+    </ul>
+    <ul class="b">
         <li><a href="login.php" class="dashboardbutton-index">DASHBOARD</a></li>
     </ul>
 </nav>
