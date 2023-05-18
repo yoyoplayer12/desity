2c0ff6e58d1b7b0454970446e3c53d4804bf5648
Yorick <yorick.devleeschouwer@gmail.com>
2023-05-18 23:37:18 +0200

    feat: 404 test

--- 404.php
+++ 404.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="icon" href="<?php echo $url."assets/brand/ppyo2h0le7ysvsembls6" ?>" style="height:40px" type="image/svg">
     <title>404 - Page not found</title>
 </head>
 <body>
